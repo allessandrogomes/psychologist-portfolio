c5a2c879c20a2253dd7ae76d48c908d53fc3df74
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-10-13 15:49:10 -0300

    :iphone:responsive: ajusta a responsividade do componente 'TitleAndSubtitle'.

--- src/components/shared/TitleAndSubtitle/index.tsx
+++ src/components/shared/TitleAndSubtitle/index.tsx
@@ -8,7 +8,8 @@ interface ITitleAndSubtitle {
 
 export default function TitleAndSubtitle({ title, subtitle, darkTheme = false }: ITitleAndSubtitle) {
     return (
-        <div className={darkTheme ? styles.darkContent : styles.content}>
+        <div className={darkTheme ? styles.darkContent : styles.content}
+        >
             <div className={styles.bar}></div>
             <h2>{title}</h2>
             <h3>{subtitle}</h3>
