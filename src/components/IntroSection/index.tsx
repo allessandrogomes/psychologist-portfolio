a28d0216b7de3c1660a04c3605ec70684ccd276c
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-11-09 22:49:16 -0300

    :sparkles:feat: adiciona a funcionalidade de scroll to section no botão 'Vamos Conversar' na IntroSection.

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import Image from "next/image"
 import styles from "./styles.module.scss"
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import XIcon from '@mui/icons-material/X'
 import TelegramIcon from '@mui/icons-material/Telegram'
+import { Link } from "react-scroll"
 
 export default function IntroSection() {
     return (
@@ -12,7 +15,20 @@ export default function IntroSection() {
                 <div className={styles.bar}></div>
                 <h1>Me chamo Alessandra Daré,<br></br>e sou Psicóloga</h1>
                 <h2>Apoio emocional e desenvolvimento pessoal para uma vida mais equilibrada</h2>
-                <button>Vamos Conversar</button>
+                <button>
+                    <Link
+                        role="button"
+                        aria-label="Scroll to respective Section"
+                        style={{ display: "block", padding: "20px 15px" }}
+                        to="contate"
+                        spy
+                        smooth
+                        duration={500}
+                        offset={0}
+                    >
+                        Vamos Conversar
+                    </Link>
+                </button>
                 <div className={styles.follow}>
                     <p>Siga-me</p>
                     <div>
