889795f5d14d3c8deb704c20ce69794573eca8b8
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-11-24 19:27:28 -0300

    :zap:perf: remove prioridade de carregamento da imagem principal.

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -42,7 +42,6 @@ export default function IntroSection() {
 
             <div className={styles.image}>
                 <Image
-                    priority
                     quality={100}
                     style={{ width: "100%", height: "100%", objectFit: "cover" }}
                     src="/images/psicologa-1.jpg"
